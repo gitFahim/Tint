e6d1113e8cfed10699f389127243fe382376ed61
gitFahim <bsse0932@iit.du.ac.bd>
2025-03-08 08:20:24 +0600

    debug

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -67,7 +67,7 @@ const Orders = ({token}) => {
                 </div>
                 <p className='mt-3 mb-2 font-medium'> {order.address.firstName + " " + order.address.lastName} </p>
                 <div> 
-                  <p>{order.address.street + ", "}</p>
+                  <p>{order.address.street + ","}</p>
                   <p>{order.address.state + ", " + order.address.city + ", " + order.address.country + ", " + order.address.zipcode}</p>
                 </div>
                 <p>{order.address.phone}</p>
